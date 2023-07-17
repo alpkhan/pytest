ac30c4c1dca98b27505acbee384503b4337c7e54
alpkhan <alperalagoz@gmail.com>
2023-07-17 20:36:57 +0300

    Update

--- Apply_to_olt.py
+++ Apply_to_olt.py
@@ -1,6 +1,20 @@
 print("Deploy")
 
 
+with open("Config_change_request.txt") as f:
+    lines = f.readlines()
+
+with open('OLT_config.txt', 'a') as f:
+    for line in lines:
+        f.write(line)
+        f.write('\n')
+
+
+
+
+
+
+
 before_list=["enable","config"," ","tcont 4 dba-profile-id 20","gem add 12 eth tcont 4 cascade on","gem mapping 12 1 vlan 300","commit","quit"," ","ont-port eth adaptive pots adaptive","commit","quit","interface gpon 0/3","ont port native-vlan 1 1 eth 1 vlan 300"]
 
 
