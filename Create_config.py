e07180f8528e7509a0f62f631549a5e1081faa3c
alpkhan <alperalagoz@gmail.com>
2023-07-17 20:28:13 +0300

    update

--- Create_config.py
+++ Create_config.py
@@ -5,15 +5,16 @@ with open("Config_change_request.txt") as f:
 
 number_of_command=0
 line_no=0
-while number_of_command>15:
-
+while number_of_command<14:
+    print(template[number_of_command])
     if template[number_of_command]==" \n":
         template[number_of_command]=lines[line_no]
         line_no+=1
     number_of_command+=1
-#print(template)
+
 
 with open('Config_change_request.txt', 'w') as f:
     for line in template:
         f.write(line)
+        
         
