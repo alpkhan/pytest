dea44a7d4e2a5760dabead68c19432654cc8150d
alpkhan <alperalagoz@gmail.com>
2023-07-17 20:22:17 +0300

    Update

--- Apply_to_olt.py
+++ Apply_to_olt.py
@@ -1,1 +1,10 @@
 print("Deploy")
+
+
+before_list=["enable","config"," ","tcont 4 dba-profile-id 20","gem add 12 eth tcont 4 cascade on","gem mapping 12 1 vlan 300","commit","quit"," ","ont-port eth adaptive pots adaptive","commit","quit","interface gpon 0/3","ont port native-vlan 1 1 eth 1 vlan 300"]
+
+
+with open('Config_change_request.txt', 'w') as f:
+    for line in before_list:
+        f.write(line)
+        f.write("\n")
--- Create_config.py
+++ Create_config.py
@@ -1,32 +1,19 @@
-import sys
-
-with open("Config_change_request.txt") as f:
+with open("Config_entry.txt") as f:
     lines = f.readlines()
-with open("ONT_SFP_template.txt") as f:
-    control_lines = f.readlines()
+with open("Config_change_request.txt") as f:
+    template = f.readlines()
 
-def remove_extra(list):
-    new_list=[]
-    
-    for element in list:
-        new_element=element.strip("\n")
-        new_list.append(new_element)
-    return new_list
+number_of_command=0
+line_no=0
+while number_of_command>15:
 
-lines=remove_extra(lines)
-control_lines=remove_extra(control_lines)
+    if template[number_of_command]==" \n":
+        template[number_of_command]=lines[line_no]
+        line_no+=1
+    number_of_command+=1
+#print(template)
 
-###Check for Enable
-if lines[0].strip("\n")!="Enable" or "enable":
-    #sys.exit(-1)
-    a=True
-### check for config test line by line
-#check for number of lines
-
-if len(lines)!=len(control_lines):
-    print("Configuation Error Number of lines does not match")
-    sys.exit(-1)
-#print(len(lines))
-for step in range(len(lines)):
-    if lines[step].find(control_lines[step])==-1:
-        sys.exit(-1)
+with open('Config_change_request.txt', 'w') as f:
+    for line in template:
+        f.write(line)
+        
